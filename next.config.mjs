b9b96f93b750a1e75c456b9c8a59a30db71a3167
Ariitea1 <afrogier@gmail.com>
2025-10-25 17:31:50 -1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
   distDir: '.next',
+  output: 'export',
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
