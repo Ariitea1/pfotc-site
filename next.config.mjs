bfed6382c5978c804753c14cf643027c0bc4666d
Ariitea1 <afrogier@gmail.com>
2025-10-25 18:01:42 -1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  output: 'standalone', // ✅ clé ici : output standalone pour Vercel
 };
 
 export default nextConfig;
