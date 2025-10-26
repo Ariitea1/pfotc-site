e85afc86b5d9d9f1e5bbe354564dffdd6476a77d
Ariitea1 <afrogier@gmail.com>
2025-10-25 17:37:01 -1000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   distDir: '.next',
   output: 'export',
-  trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
